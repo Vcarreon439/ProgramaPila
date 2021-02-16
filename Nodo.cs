e463f1f0b01f6dbe3c7dc8a2a4a2da43ec9cd830
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2021-02-16 13:46:35 -0600

    Se agrego clase nodo para el correcto funcionamiento del programa pilas

---
+++ Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaPila
+{
+    class Nodo
+    {
+        private Object dato;
+        private Nodo siguiente = null;
+
+        public object Dato { get => dato; set => dato = value; }
+        public Nodo Siguiente { get => siguiente; set => siguiente = value; }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}]", dato);
+        }
+
+    }
+}
